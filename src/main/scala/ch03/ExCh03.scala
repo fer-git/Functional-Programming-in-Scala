880b37608628892d54b378a0cede70475c3cfcf4
Fernando Pratama <fernando.pratama.1290@gmail.com>
2016-05-12 16:31:12 +0800

    - Finish up to exercise 6 Chapter03

--- src/main/scala/ch03/ExCh03.scala
+++ src/main/scala/ch03/ExCh03.scala
@@ -34,9 +34,41 @@ object List {
     case Cons(_, xs) => xs
   }
 
-  def drop[A](lst: List[A], n: Int): List[A] = ???
+  /**
+    * Ex03, Drop n many elements from a list
+    * @param lst List operated on
+    * @param n Number of elements need to be dropped from the beginning
+    *          of the list
+    * @return List with dropped n elements
+    */
+  def drop[A](lst: List[A], n: Int): List[A] = (lst, n) match {
+    case (Nil, _) => Nil
+    case (xs, k) if k <= 0 => xs
+    case (Cons(x, xs), k) => drop(xs, k - 1)
+  }
 
-  def dropWhile[A](lst: List[A])(f: A => Boolean): List[A] = ???
+  /**
+    * Ex04, Removes elements from the List prefix as long as they match a
+    * predicate
+    * @param lst List operated on
+    * @param f Predicate function for each element
+    * @return The remaining elements of a list where the first element is the
+    *         first time the predicate function on that element is false
+    */
+  def dropWhile[A](lst: List[A])(f: A => Boolean): List[A] = lst match {
+    case Nil => Nil
+    case list @ Cons(x, xs) if !f(x) => list
+    case Cons(_, xs) => dropWhile(xs)(f)
+  }
 
-  def setHead[A](lst: List[A], x: A): List[A] = ???
+  /**
+    * Replace the first element of a List with different value
+    * @param lst List operated on
+    * @param x Replacement on the first element
+    * @return The list with first element replaced with parameter x
+    */
+  def setHead[A](lst: List[A], x: A): List[A] = lst match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(x, t)
+  }
 }
--- src/test/scala/ch03Test/ExCh03Test.scala
+++ src/test/scala/ch03Test/ExCh03Test.scala
@@ -28,25 +28,48 @@ class ExCh03Test extends FunSpec with Matchers {
 
   describe("drop method on List") {
     it("Should return empty list for dropping elements of empty list") {
-      List.drop(emptyList, 2) should equal(emptyList)
+      List.drop(emptyList, 2) should equal (emptyList)
     }
-
+    it("Should return the same list for dropping negative number of elements") {
+      List.drop(intList, -10) should equal (intList)
+    }
     it("Should behave like tail when dropping one element") {
       List.drop(intList, 1) should equal (List[Int](2, 3))
     }
-
     it("Should drop 2 elements if n = 2") {
       List.drop(charList, 2) should equal (List[Char]('c'))
     }
   }
 
   def alwaysTrue[A](x: A): Boolean = true
-  def isEven(x: Int): Boolean = (x % 2) == 0
+  def alwaysFalse[A](x: A): Boolean = false
+  def isOdd(x: Int): Boolean = (x % 2) == 1
 
   describe("dropWhile method on List") {
     it("Should return empty list for dropWhile on empty list") {
       List.dropWhile(emptyList)(alwaysTrue) should equal (Nil)
     }
-    it("Should return empty list if the predicate of each element is wrong")
+    it("Should return the same list if the predicate on each element is " +
+      "wrong") {
+      List.dropWhile(intList)(alwaysFalse) should equal (intList)
+    }
+    it("Should return the same list if the predicate on each element is true") {
+      List.dropWhile(intList)(alwaysTrue) should equal (Nil)
+    }
+    it("Should stop taking element once the predicate is wrong") {
+      List.dropWhile(intList)(isOdd) should equal (List[Int](2, 3))
+    }
+  }
+
+  describe("setHead method on List") {
+    it("Should return an empty list if setHead applied on empty list") {
+      List.setHead(emptyList, 1) should equal (emptyList)
+    }
+    it("Should return list with replaced first element on integer list") {
+      List.setHead(intList, 10) should equal (List[Int](10, 2, 3))
+    }
+    it("Should return list with replaced first element on char list") {
+      List.setHead(charList, 'z') should equal (List[Char]('z', 'b', 'c'))
+    }
   }
 }
