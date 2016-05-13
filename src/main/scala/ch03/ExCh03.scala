39d29495b6bc7bb311e57a6a3406660cc44ce87f
Fernando Pratama <fernando.pratama.1290@gmail.com>
2016-05-13 14:20:57 +0800

    - Finish up to exercise 11 Chapter03

--- src/main/scala/ch03/ExCh03.scala
+++ src/main/scala/ch03/ExCh03.scala
@@ -82,4 +82,51 @@ object List {
     case Cons(x, Nil) => Nil
     case Cons(x, xs) => Cons(x, init(xs))
   }
+
+  def foldRight[A, B](l: List[A], z: B)(f: (A, B) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  def sum2(l: List[Int]) = foldRight(l, 0)(_ + _)
+
+  def product2(l: List[Double]) = foldRight(l, 1.0) (_ * _)
+
+  /**
+    * Ex09, Compute the length of a list using foldRight
+    * @param l list operated on
+    * @return The length of the list
+    */
+  def length[A](l: List[A]): Int = foldRight(l, 0)((x: A, z: Int) => z + 1)
+
+  /**
+    * Ex10, Alternative to foldRight, consume the list from left to right
+    * @param l List operated on
+    * @param z Accumulator
+    * @param f Function that accepts two parameters, the first one is for
+    *          accumulator, the second one is for the current element on list
+    * @return Reduced operation on a list l with function f and initial
+    *         accumulator z
+    */
+  def foldLeft[A, B](l: List[A], z: B)(f:(B, A) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
+
+  /*
+  Ex11, rewrite sum, product, and length using foldLeft
+   */
+  def sum3(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
+  def product3(l: List[Double]): Double = foldLeft(l, 1.0)(_ * _)
+  def length2[A](l: List[A]): Int = foldLeft(l, 0)((z: Int, x: A) => z + 1)
+
+  def reverse[A](l: List[A]): List[A] = ???
+
+  def foldLeft2[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
+
+  def appendFL[A](l: List[A], xs: List[A]): List[A] = ???
+
+  def appendFR[A](l: List[A], xs: List[A]): List[A] = ???
+
+  def flatMap[A](ll: List[List[A]]): List[A] = ???
 }
--- src/test/scala/ch03Test/ExCh03Test.scala
+++ src/test/scala/ch03Test/ExCh03Test.scala
@@ -87,4 +87,112 @@ class ExCh03Test extends FunSpec with Matchers {
       List.init(charList) should equal (List[Char]('a', 'b'))
     }
   }
+
+  describe("length method on List") {
+    it("Should return 0 for an empty list") {
+      List.length(emptyList) should equal (0)
+    }
+    it("Should return 3 for integer list with 3 elements") {
+      List.length(intList) should equal (3)
+    }
+    it("Should return 3 for char list with 3 elements") {
+      List.length(charList) should equal (3)
+    }
+  }
+
+  describe("foldLeft method on List") {
+    it("Should return accumulator for an empty list") {
+      List.foldLeft(emptyList, 0)((x: Int, y: Int) => x + y) should equal (0)
+    }
+    it("Should return sum of each element in integer list") {
+      List.foldLeft(intList, 0)((x: Int, y: Int) => x + y) should equal (6)
+    }
+    it("Should return string of concatenated char in char list") {
+      List.foldLeft(charList, "")((x: String, y: Char) => x + y) should
+        equal ("abc")
+    }
+  }
+
+  val doubleList = List[Double](1.0, 2.0, 3.0)
+  describe("foldLeft version of sum, product, and length on List") {
+    it("sum3 should return the same result as its foldRight version, sum2") {
+      List.sum3(intList) should equal (List.sum2(intList))
+    }
+    it("product3 should return the same result as its foldRight version, " +
+      "product2") {
+      List.product3(doubleList) should equal (List.product2(doubleList))
+    }
+    it("length2 should return the same result as its foldRight version, " +
+      "length") {
+      List.length2(charList) should equal (List.length(charList))
+    }
+  }
+
+  describe("reverse method on List") {
+    it("Should return empty list for reverse on empty list") {
+      List.reverse(emptyList) should equal (Nil)
+    }
+    it("Should return list with reversed element for reverse on integer list") {
+      List.reverse(intList) should equal (List[Int](3, 2, 1))
+    }
+    it("Should return list with reversed element for reverse on char list") {
+      List.reverse(charList) should equal (List[Char]('c', 'b', 'a'))
+    }
+  }
+
+  describe("foldLeft2 method on List") {
+    it("should return the same result as foldLeft on empty list") {
+      List.foldLeft2(emptyList, 0)((x: Int, y: Int) => x + y) should
+        equal (List.foldLeft(emptyList, 0)((x: Int, y: Int) => x + y))
+    }
+    it("should return the same result as foldLeft on integer list") {
+      List.foldLeft2(intList, 0)((x: Int, y: Int) => x + y) should
+        equal (List.foldLeft(intList, 0)((x: Int, y: Int) => x + y))
+    }
+    it("should return the same result as foldRight on char list") {
+      List.foldLeft2(charList, "")((x: String, y: Char) => x + y) should
+        equal (List.foldLeft(charList, "")((x: String, y: Char) => x + y))
+    }
+  }
+
+  describe("appendFL method on List") {
+    it("should return the same list if it is appended by empty list") {
+      List.appendFL(intList, emptyList) should equal (intList)
+    }
+    it("should return a list with combined elements on integer list") {
+      List.appendFL(intList, intList) should equal(List[Int](1, 2, 3, 1, 2, 3))
+    }
+    it("should return a list with combined elements on char list") {
+      List.appendFL(charList, charList) should equal (List[Char]('a', 'b',
+        'c', 'a', 'b', 'c'))
+    }
+  }
+
+  describe("appendFR method on List") {
+    it("should return the same list if it is appended by empty list") {
+      List.appendFR(intList, emptyList) should equal(intList)
+    }
+    it("should return a list with combined elements on integer list") {
+      List.appendFR(intList, intList) should equal(List[Int](1, 2, 3, 1, 2, 3))
+    }
+    it("should return a list with combined elements on char list") {
+      List.appendFR(charList, charList) should equal(List[Char]('a', 'b',
+        'c', 'a', 'b', 'c'))
+    }
+  }
+
+  describe("flatMap method on List") {
+    it("should return empty list for flatMap on list that contains empty " +
+      "list") {
+      List.flatMap(List(emptyList)) should equal (Nil)
+    }
+    it("should concatenated a list of two integer lists into an integer list") {
+      List.flatMap(List(intList, intList)) should equal (List[Int](1, 2, 3,
+        1, 2, 3))
+    }
+    it("should concatenated a list of two char lists into a char list") {
+      List.flatMap((List(charList, charList))) should equal (List[Char]('a',
+        'b', 'c', 'a', 'b', 'c'))
+    }
+  }
 }
