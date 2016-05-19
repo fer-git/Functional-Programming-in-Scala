2d74684eaf90471c409c17e63cba141ab306cb08
Fernando Pratama <fernando.pratama.1290@gmail.com>
2016-05-20 01:11:50 +0800

    - Finish up to exercise 4 Chapter04

--- src/main/scala/ch04/ExCh04.scala
+++ src/main/scala/ch04/ExCh04.scala
@@ -1,11 +1,14 @@
 package ch04
 
+import java.util.regex.{PatternSyntaxException, Pattern}
+
 /**
   * Created by fernandopratama on 18/5/16.
   */
 object ExCh04 {
   /**
     * Compute the mean of a sequence
+    *
     * @param xs Sequence operated on
     * @return The mean of xs
     */
@@ -16,6 +19,7 @@ object ExCh04 {
 
   /**
     * Ex02, Implement variance function in terms of mean and flatMap
+    *
     * @param xs Sequence operated on
     * @return The variance of xs
     */
@@ -27,6 +31,7 @@ object ExCh04 {
     * Ex03, Combine two Option values using a binary function. If either Option
     * value
     * is None then return value is also None
+    *
     * @param a first Option
     * @param b second Option
     * @param f function to be applied on values of a and b
@@ -39,7 +44,46 @@ object ExCh04 {
     } yield f(valA, valB)
   }
 
-  def bothMatch_2(pat1: String, pat2: String, s: String): Option[Boolean] = ???
+  def pattern(s: String): Option[Pattern] = {
+    try {
+      Some(Pattern.compile(s))
+    } catch {
+      case e: PatternSyntaxException => None
+    }
+  }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+  def mkMatcher(pat: String): Option[String => Boolean] = {
+    for{
+      p <- pattern(pat)
+    } yield (s: String) => p.matcher(s).matches
+  }
+
+  def doesMatch(pat: String, s: String): Option[Boolean] = {
+    for {
+      p <- mkMatcher(pat)
+    } yield p(s)
+  }
+
+  /**
+    * Ex04, Reimplement bothMatch in terms of map2 function
+    *
+    * @param pat1 First pattern
+    * @param pat2 Second Pattern
+    * @param s String to be matched by pat1 and pat2
+    * @return True if both pattern match the string s, else false
+    */
+  def bothMatch_2(pat1: String, pat2: String, s: String): Option[Boolean] =
+    map2(doesMatch(pat1, s), doesMatch(pat2, s))((x, y) => x && y)
+
+  /**
+    * Ex05, Combine a list of Options into one option containing a list of
+    * all the Some values in the original list
+    *
+    * @param a List of Option
+    * @return List of Some from original list, if the original list contains
+    *         at least one None, then the return is None
+    */
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_ :: _))
+  }
 }
--- src/test/scala/ch04Test/ExCh04Test.scala
+++ src/test/scala/ch04Test/ExCh04Test.scala
@@ -12,20 +12,47 @@ class ExCh04Test extends FunSpec with Matchers {
   val doubleList = List[Double](1.0, 2.0, 3.0, 4.0)
 
   describe("mean") {
-    it("Should return None for empty list") {
+    it("should return None for empty list") {
       mean(emptyList) should equal (None)
     }
-    it("Should return the mean for Double List") {
+    it("should return the mean for Double List") {
       mean(doubleList) should equal (Some(2.5))
     }
   }
 
   describe("variance") {
-    it("Should return None for empty list") {
+    it("should return None for empty list") {
       variance(emptyList) should equal (None)
     }
-    it("Should return the variance for Double List") {
+    it("should return the variance for Double List") {
       variance(doubleList) should equal (Some(1.25))
+    }
+  }
+
+  describe("bothMatch_2") {
+    it("should return false if both patterns are wrong") {
+      bothMatch_2("scala", "clojure", "python") should equal (Some(false))
+    }
+    it("should return false if one of the pattern is wrong") {
+      bothMatch_2("scala", "clojure", "scala") should equal (Some(false))
+    }
+    it("should return true of both patterns are true") {
+      bothMatch_2("(^hello)", "hello$", "hello") should
+        equal (Some(true))
+    }
+  }
+
+  describe("sequence") {
+    it("should return Some(Nil) for empty list") {
+      sequence(Nil) should equal (Some(Nil))
+    }
+    it("should return Some of list which contains the value of original " +
+      "list") {
+      sequence(List(Some(1), Some(2), Some(3))) should equal (Some(List(1, 2,
+        3)))
+    }
+    it("should return None if the list contains None") {
+      sequence(List(Some(1), None, Some(3))) should equal (None)
     }
   }
 }
