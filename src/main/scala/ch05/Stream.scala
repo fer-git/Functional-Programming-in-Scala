8bde631da9a39f2db790e02da786bc0109f4ae2b
Fernando Pratama <fernando.pratama.1290@gmail.com>
2016-05-22 23:56:43 +0800

    - Finish up to exercise 10 Chapter05

--- src/main/scala/ch05/Stream.scala
+++ src/main/scala/ch05/Stream.scala
@@ -38,6 +38,54 @@ sealed trait Stream[+A] {
     case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
     case _ => empty
   }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  /**
+    * Ex04, Implement forAll, which checks that all element in the Stream
+    * match a
+    * given predicate
+    * @param f Function to be applied on each element of the Stream
+    * @return True if predicate of each element is true, else false
+    */
+  def forAll(f: A => Boolean): Boolean =
+    foldRight(true)((a, b) => f(a) && b)
+
+  /**
+    * Ex05, use foldRight to implement takeWhile
+    * @param p Predicate function to be appleid on each element of the Stream
+    * @return Starting elements in which the predicate of the element is true
+    */
+  def takeWhileViaFoldRight(p: A => Boolean): Stream[A] = {
+    foldRight(empty[A])((h, t) =>
+    if (p(h)) cons(h, t)
+    else empty)
+  }
+
+  /*
+  Ex06, Implement map, filter, append and flatMap using foldRight
+   */
+  def map[B](f: A => B): Stream[B] = {
+    foldRight(empty[B])((h, t) => cons(f(h), t))
+  }
+
+  def filter(f: A => Boolean): Stream[A] = {
+    foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else t)
+  }
+
+  def append[B>:A](s: => Stream[B]): Stream[B] = {
+    foldRight(s)((h, t) => cons(h, t))
+  }
+
+  def flatMap[B>:A](f: A => Stream[B]): Stream[B] = {
+    foldRight(empty[B])((h, t) => f(h) append t)
+  }
 }
 
 case object Empty extends Stream[Nothing]
@@ -55,5 +103,49 @@ object Stream {
   def apply[A](as: A*): Stream[A] = {
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
+  }
+
+  /**
+    * EX07, Return an infinite stream of a given values
+    * @param a The element in the Stream
+    * @return Stream with element a
+    */
+  def constant[A](a: A): Stream[A] = {
+    lazy val tail: Stream[A] = Cons(() => a, () => tail)
+    tail
+  }
+
+  /**
+    * Ex08, Generate infinite stream of integers
+    * @param n The starting value of the Stream
+    * @return Stream with elements started with n, and incremented by 1 on
+    *         the next element
+    */
+  def from(n: Int): Stream[Int] = {
+    cons(n, from(n + 1))
+  }
+
+  /**
+    * Ex09, Generate the infinite stream of Fibonacci number
+    */
+  val fibs = {
+    def go(f0: Int, f1: Int): Stream[Int] = {
+      cons(f0, go(f1, f0 + f1))
+    }
+    go(0, 1)
+  }
+
+  /**
+    * Ex10, General stream building function. Take an initial state, and
+    * function for producing both the next value and state in the
+    * generated stream
+    * @param z Initial State
+    * @param f Function to produce next value and state in generated
+    *          stream
+    * @return Stream generated with initial value z, and function f
+    */
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case Some((h, t)) => cons(h, unfold(t)(f))
+    case None => empty
   }
 }
--- src/test/scala/ch05Test/StreamTest.scala
+++ src/test/scala/ch05Test/StreamTest.scala
@@ -35,4 +35,105 @@ class StreamTest extends FunSpec with Matchers {
         equal (Stream(1, 3, 5).toList)
     }
   }
+
+  describe("forAll method in Stream") {
+    it("should return true for Empty Stream") {
+      Empty.forAll(x => false) should equal (true)
+    }
+    it("should return false if the predicate of at least one the element is " +
+      "false") {
+      Stream(1, 2, 3).forAll(x => x%2 == 1) should equal (false)
+    }
+    it("should return true if the predicate of all elements are true") {
+      Stream(2, 4, 6).forAll(x => x%2 == 0) should equal (true)
+    }
+  }
+
+  describe("takeWhileViaFoldRight method in Stream") {
+    it("should return Empty for Empty Stream") {
+      Empty.takeWhileViaFoldRight(x => true) should equal (Empty)
+    }
+    it("should return all starting elements which predicate return true") {
+      Stream(1, 3, 5, 4, 7).takeWhileViaFoldRight(x => x%2 == 1).toList should
+        equal (Stream(1, 3, 5).toList)
+    }
+  }
+
+  describe("map method in Stream") {
+    it("should return Empty for Empty Stream") {
+      Empty.map(x => true) should equal (Empty)
+    }
+    it("should return Cons with each element applied by function on Cons") {
+      Stream(1, 2, 3).map((x: Int) => x + 1).toList should
+        equal(Stream(2, 3, 4).toList)
+    }
+  }
+
+  describe("filter method in Stream") {
+    it("should return Empty for Empty Stream") {
+      Empty.filter(x => true) should equal (Empty)
+    }
+    it("should return Empty if predicate on each element is false") {
+      Stream(2, 4, 6).filter(_%2 == 1) should equal (Empty)
+    }
+    it("should return Stream only if the predicate on element is true") {
+      Stream(1, 2, 3).filter(_%2 == 1).toList should equal (Stream(1, 3).toList)
+    }
+  }
+
+  describe("append method in Stream") {
+    it("should return original Stream if appended by Empty") {
+      Stream(1, 2, 3).append(Empty).toList should equal (Stream(1, 2, 3).toList)
+    }
+    it("should append two Streams into one Stream") {
+      Stream(1, 2, 3).append(Stream(4, 5)).toList should
+      equal (Stream(1, 2, 3, 4, 5).toList)
+    }
+  }
+
+  describe("flatMap method in Stream") {
+    it("should return Empty for Empty Stream") {
+      Empty.flatMap(x => Stream(1)) should equal (Empty)
+    }
+    it("should return Stream with each element applied by function and " +
+      "flatted") {
+      Stream(1, 2, 3).flatMap(x => Stream(x + 1)).toList should equal (Stream
+        (2, 3, 4).toList)
+    }
+  }
+
+  describe("constant method in Stream") {
+    it("should return Stream with the same elements") {
+      Stream.constant(10).take(3).toList should equal (Stream(10, 10, 10)
+        .toList)
+    }
+  }
+
+  describe("from method in Stream") {
+    it("should return Stream with increasing sequence starting from -1") {
+      Stream.from(-1).take(3).toList should equal(Stream(-1, 0, 1).toList)
+    }
+  }
+
+  describe("fibs in Stream") {
+    it("should return Stream with one element from fibonacci series") {
+      Stream.fibs.take(1).toList should equal (List(0))
+    }
+    it("should return Stream with 3 elements from fibonacci series") {
+      Stream.fibs.take(3).toList should equal (List(0, 1, 1))
+    }
+  }
+
+  describe("unfold in Stream") {
+    it("should return Stream with increasing sequence starts 1") {
+      Stream.unfold(1)(x => Some((x, x+1))).take(3).toList should
+        equal (Stream(1, 2, 3).toList)
+    }
+    it("should return Stream with increasing number of s") {
+      Stream.unfold("x")(x => Some((x, x + "s"))).take(3).toList should
+        equal (Stream("x", "xs", "xss").toList)
+    }
+  }
+
+
 }
